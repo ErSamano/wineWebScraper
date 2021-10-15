301c8bc31b73b77f017c41929eda279a89690325
bot-2 <Papadophulus@gmail.com>
2021-10-15 16:32:53 +0000

    Improve the code with a CSV save mode

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import time
 
 '''
 When the content page is dynamic, we need to anable a way to load
-the hole html site. To achieve that we are going to use Selenium Lib
+the hiden html site content. In order to achieve that we are going 
+to use Selenium Lib
 to load dynamic elements.
 '''
 s=Service(ChromeDriverManager().install())
@@ -18,15 +19,15 @@ driver.get(url)
 
 #page = driver.execute_script('return document.body.innerHTML')
 
+# Setting up a waiting time before scroll down
 SCROLL_PAUSE_TIME = 0.5
+
 # Get scroll height
 last_height = driver.execute_script("return document.body.scrollHeight")
-print(last_height)
 
 while True:
     # Scroll down to bottom
     time.sleep(SCROLL_PAUSE_TIME)
-
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     # Wait to load page
@@ -41,40 +42,40 @@ while True:
 
 res = driver.execute_script("return document.documentElement.outerHTML")
 driver.quit()
-soup = BeautifulSoup(res, 'html.parser')
+soup = BeautifulSoup(res, 'lxml')
 
 products_names = []
 products_link = []
 products_price = []
 
-#Loop to bring all the products name
+# Get all the products name
 for contentName in soup.findAll(attrs={'class':'product-item__name'}):
     # Get the Product Name
     name_desc = contentName.find('a')
     products_names.append(name_desc.text)
-    print(products_names)
 
-#Loop to bring all the products link
+# Get all the products link
 for contentLink in soup.findAll(attrs={'class':'product-item__name'}):
     # Get the Product Link
     for links in contentLink.find_all('a'):
         product_link = links.get('href')
         products_link.append(product_link)
-        print(products_link)
 
-#Loop to bring all the products price
+# Get all the products price
 for contentPrice in soup.findAll(attrs={'class':'product-item__price'}):
     # Get the Product Name
-    product_price = contentPrice.find(class_="price-promo")
-    products_price.append(product_price)
-    print(products_price)
-
+    # Check if product is still available
+    if contentPrice.find(class_="price-promo"):
+        product_price = contentPrice.find(class_="price-promo")
+        products_price.append(product_price.text)
+    else:
+        products_price.append("AGOTADO")
 
 table = {'Nombre': products_names,
          'Links': products_link,
          'Precios': products_price
         }
 full_table = pd.DataFrame(table)
-#full_table.to_csv('lista.csv')
-print("Full table:\n")
-print(full_table)
+full_table.to_csv('lista.csv')
+# print("Full table:\n")
+# print(full_table)
